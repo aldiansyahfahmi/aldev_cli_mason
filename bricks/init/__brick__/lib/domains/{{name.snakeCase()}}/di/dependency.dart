d6477b0eaa2bf082df4367d66933b581429cd955
aldiansyahfahmi <aldiansyahfahmi6@gmail.com>
2023-08-05 21:32:19 +0800

    update

---
+++ bricks/init/__brick__/lib/domains/{{name.snakeCase()}}/di/dependency.dart
@@ -0,0 +1,24 @@
+class {{name.pascalCase()}}Dependency {
+  {{name.pascalCase()}}Dependency() {
+    _registerDataSource();
+    _registerMapper();
+    _registerRepository();
+    _registerUseCases();
+  }
+
+  void _registerDataSource() {
+
+  }
+
+  void _registerMapper() {
+
+  }
+
+  void _registerRepository() {
+
+  }
+
+  void _registerUseCases() {
+    
+  }
+}
